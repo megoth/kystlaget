3010e44f149a721d827165c4e72fb5ab7fea2120
Arne Hassel <arne.hassel@gmail.com>
2023-05-04 16:58:51 +0200

    Foolproofed when no image is given for group members

--- components/group/member/association/index.tsx
+++ components/group/member/association/index.tsx
@@ -1,20 +1,20 @@
-import { asFullSize } from '../../../../lib/images';
+import {asFullSize} from '../../../../lib/images';
 import React from 'react';
-import { imageBuilder } from '../../../../lib/sanity';
-import { imageStyle, nameStyle } from './styles.css';
+import {imageBuilder} from '../../../../lib/sanity';
+import {imageStyle, nameStyle} from './styles.css';
 
 interface GroupMemberAssociationProps {
-  association: Sanity.Schema.Association
+    association: Sanity.Schema.Association
 }
 
-export default function GroupMemberAssociation({ association }: GroupMemberAssociationProps) {
-  const image = imageBuilder(association.logo);
-  return <div>
-    <img
-      className={imageStyle}
-      src={asFullSize(image).url()}
-      alt={`Logo for ${association.name}`}
-    />
-    <div className={nameStyle}>{association.name}</div>
-  </div>
+export default function GroupMemberAssociation({association}: GroupMemberAssociationProps) {
+    const image = association.logo && imageBuilder(association.logo);
+    return <div>
+        {image && <img
+            className={imageStyle}
+            src={asFullSize(image).url()}
+            alt={`Logo for ${association.name}`}
+        />}
+        <div className={nameStyle}>{association.name}</div>
+    </div>
 }
--- components/group/member/person/index.tsx
+++ components/group/member/person/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { imageBuilder } from '../../../../lib/sanity';
-import { asSquare } from '../../../../lib/images';
-import { imageStyle, nameStyle, personStyle } from './styles.css';
+import {imageBuilder} from '../../../../lib/sanity';
+import {asSquare} from '../../../../lib/images';
+import {imageStyle, nameStyle, personStyle} from './styles.css';
 
 interface GroupMemberPerson {
-  person: Sanity.Schema.Person
+    person: Sanity.Schema.Person
 }
 
-export default function GroupMemberPerson({ person }: GroupMemberPerson) {
-  const image = imageBuilder(person.image);
-  return (
-    <div className={personStyle}>
-      <img
-        className={imageStyle}
-        src={asSquare(image).url()}
-        alt={`Bilde av ${person.name}`}
-      />
-      <div className={nameStyle}>{person.name}</div>
-    </div>
-  )
+export default function GroupMemberPerson({person}: GroupMemberPerson) {
+    const image = person.image && imageBuilder(person.image);
+    return (
+        <div className={personStyle}>
+            {image && <img
+                className={imageStyle}
+                src={asSquare(image).url()}
+                alt={`Bilde av ${person.name}`}
+            />}
+            <div className={nameStyle}>{person.name}</div>
+        </div>
+    )
 }
